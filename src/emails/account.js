a8bbbe76b520e4070890c5c161161a1ba7f94ee3
vijay3778 <dididivya999@gmail.com>
2020-01-20 15:34:54 +0530

    init commit2

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,37 +1,26 @@
 const sgMail =  require('@sendgrid/mail')
 
-sgMail.setApiKey('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')
+sgMail.setApiKey(SENDGRID_API_KEY)
 
-//sgMail.setApiKey('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')
+const sendWelcomeEmail =   (email, name) => {
+    sgMail.send({
+        to : email, 
+        from : 'pleasedonthackmymails@gmail.com',
+        subject : 'thanks for joining in!',
+        text :`welcome to the app, ${name}. let me know how you get along with the app `
+    })
+}
 
-// const sendWelcomeEmail =   (email, name) => {
-//     //console.log(sendgridAPIKey)
-//     sgMail.send({
-//         to : email, 
-//         from : 'pleasedonthackmymails@gmail.com',
-//         subject : 'thanks for joining in!',
-//         text :`welcome to the app, ${name}. let me know how you get along with the app `
-//     })
-// }
+const sendCancelEmail =   (email, name) => {
+    sgMail.send({
+        to : email, 
+        from : 'pleasedonthackmymails@gmail.com',
+        subject : 'thank you for using our services!',
+        text :`${name} please let us know your experience and what we could not help you with `
+    })
+}
 
-// const sendCancelEmail =   (email, name) => {
-//     sgMail.send({
-//         to : email, 
-//         from : 'pleasedonthackmymails@gmail.com',
-//         subject : 'thank you for using our services!',
-//         text :`${name} please let us know your experience and what we could not help you with `
-//     })
-// }
-
-
-sgMail.send({
-    to : 'pleasedonthackmymails@gmail.com',
-    from : 'pun.unipune.ac.in@someoneisfuckingwithyou',
-    subject : 'EGR online exam ',
-    text : 'it has been found that you have already failed in EGR offline exam in you re-exam so you cannot attemp EGR online exam, for more details please concern your administration'
-})
-
-// module.exports = {
-//     sendWelcomeEmail,
-//     sendCancelEmail
-// }
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelEmail
+}
